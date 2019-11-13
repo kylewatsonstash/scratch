460619fa32bc0f02a8f0caea8d1cb7c37743f3e3
Kyle <kwatson@stashinvest.com>
2019-11-13 11:41:17 -0500

    second commit

--- helloworld.rb
+++ helloworld.rb
@@ -1,1 +1,9 @@
-puts "hello world"
+i = 1
+while i < 11
+	puts "this is loop #{i}"
+	i += 1
+end
+
+
+ruby helloworld.rb
+
---
+++ homework1.rb
@@ -0,0 +1,9 @@
+#Day 1 Homework: Make a new file that prints the same string 
+#10 times and commit it to the same GitHub repository we created
+
+i = 1
+while i < 11
+	puts "this is loop #{i}"
+	i += 1
+end
+

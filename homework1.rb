82f477a4ef54470915650d5ea2e3e3a9daff4c2b
Kyle <kwatson@stashinvest.com>
2019-11-14 16:52:11 -0500

    updated to add different loop type

--- homework1.rb
+++ homework1.rb
@@ -1,13 +1,13 @@
 #Day 1 Homework: Make a new file that prints the same string 
 #10 times and commit it to the same GitHub repository we created
 
-i = 1
-while i < 11
-	puts "this is loop #{i}"
-	i += 1
-end
+# i = 1
+# while i < 11
+# 	puts "this is loop #{i}"
+# 	i += 1
+# end
 
 10.times do |i|
-	puts "this is loop #{i+1}"
+	puts "this is second instance of running loop #{i+1}"
 end
 

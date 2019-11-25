1970dbac68a8ddd8c0ccde73a9ae5883a9ef2b7a
Kyle <kwatson@stashinvest.com>
2019-11-25 15:21:50 -0500

    updated to remove brackets and parens

--- homework2_stringinterp.rb
+++ homework2_stringinterp.rb
@@ -3,16 +3,12 @@
 
 def this_program(number)
 
-(
 puts "If I wanted to I could tell this dumb program to print #{number} many times...\nbut instead it will only show once. Haha!"
 puts
-	[
-	number.times do
-	puts "Nevermind. I'll print this line #{number} times."
-	end
-	]
-)
-
+	number.times do |i|
+	puts "Nevermind. I'll print this line #{number} times. Printing time # #{i+1}"
+end
+puts
 end
 
 this_program(5)
